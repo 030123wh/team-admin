2b09478ac3f94b26437f13a7aaba112ebc675c3d
jjjjjjjhhhhh <2483152270@qq.com>
2024-10-11 18:27:44 +0800

    搭建express

---
+++ server.js
@@ -0,0 +1,25 @@
+const express=require("express");
+const bodyParser=require("body-parser");
+const path=require("path");
+const app=express();
+var {createProxyMiddleware } = require("http-proxy-middleware");
+var proxyPath = "http://127.0.0.1:3000";
+var proxyOption ={target:proxyPath,changeOrigoin:true};
+
+app.use(bodyParser.urlencoded({extended:true}))
+app.use(bodyParser.json());
+
+app.use(express.static(__dirname));
+
+app.get("/",(reg,res)=>{
+    res.sendFile(path.join(__dirname,"index.html"));
+})
+
+app.get("/form",(reg,res)=>{
+    res.sendFile(path.join(__dirname,"form.html"));
+})
+app.use("/api",createProxyMiddleware(proxyOption))
+
+app.listen(8080,()=>{
+    console.log("server is running on port 8080");
+})
